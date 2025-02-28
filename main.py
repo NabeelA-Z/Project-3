19629631e9ec1732357acbd43feece8a4fcf7f28
Omar A <arshao1@mcmaster.ca>
2025-02-28 13:59:07 -0500

    removed comments

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ model = YOLO('yolo11n.pt')
 def get_image_from_url(url):
     try:
         response = requests.get(url)
-        image_array = np.asarray(bytearray(response.content), dtype=np.uint8) # numpy byte array
+        image_array = np.asarray(bytearray(response.content), dtype=np.uint8) # numpy image byte array
         image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
 
         return image
@@ -35,15 +35,3 @@ def get_objects(url, showimage=True):
 
 get_objects("https://media.discordapp.net/attachments/1286060182866104423/1344380810509287564/image.jpg?ex=67c0b3b2&is=67bf6232&hm=1c170ff57b0a03efc390bdcc140b8c39438feff7527ed152879a6970fa8e13fa&=&format=webp&width=556&height=741")
 
-#image = load_image_bgr("https://media.discordapp.net/attachments/1286060182866104423/1344380810509287564/image.jpg?ex=67c0b3b2&is=67bf6232&hm=1c170ff57b0a03efc390bdcc140b8c39438feff7527ed152879a6970fa8e13fa&=&format=webp&width=556&height=741")
-#from inference.core.utils.image_utils import load_image_bgr
-#from inference import get_model
-#import supervision as sv
-"""model = get_model(model_id="yolov8n-640")
-results = model.infer(image)[0]
-results = sv.Detections.from_inference(results)
-annotator = sv.BoxAnnotator(thickness=2)
-annotated_image = annotator.annotate(image, results)
-annotator = sv.LabelAnnotator(text_scale=1, text_thickness=1)
-annotated_image = annotator.annotate(annotated_image, results)
-#sv.plot_image(annotated_image)"""
